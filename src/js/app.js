e7a988cba38498f062ef407170fa3413a4ae81d3
anabartos <ana.l.bartos@gmail.com>
2022-05-31 19:29:32 +0000

    Subo ejercicio conditional-profile

--- src/js/app.js
+++ src/js/app.js
@@ -32,10 +32,12 @@ function render(variables = {}) {
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <img src="${variables.avatarURL}" class="photo"/>
+          <h1>${variables.name ? variables.name : ""} ${
+    variables.lastname ? variables.lastname : ""
+  }</h1>
+          <h2>${variables.role ? variables.role : ""}</h2>
+          <h3>${variables.city ? variables.city : ""}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
